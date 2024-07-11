3cb410f01b60406d89393c1a58cc748fdc0249ab
Bailly Rurangirwa <rubailly@gmail.com>
2024-07-11 15:34:40 +0200

    Refactored Visa payment service to handle MTN Mobile Money transactions and updated REN format transformation logic.

--- src/main/java/com/aetrade/paymentbridge/service/VisaPaymentService.java
+++ src/main/java/com/aetrade/paymentbridge/service/VisaPaymentService.java
@@ -2,6 +2,12 @@ package com.aetrade.paymentbridge.service;
 
 import com.aetrade.paymentbridge.model.PaymentRequest;
 import com.aetrade.paymentbridge.model.PaymentResponse;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,68 +15,67 @@ public class VisaPaymentService implements GatewayService {
 
     @Override
     public PaymentResponse processPayment(PaymentRequest request) {
-        // Implementation specific to Visa
         PaymentResponse response = new PaymentResponse();
         response.setStatus("APPROVED");
         response.setResponseCode("200");
-        response.setResponseMessage("Visa transaction successful");
+        response.setResponseMessage("MTN Mobile Money transaction successful");
         return response;
     }
 
     @Override
     public void processCallback(PaymentResponse response) {
-        // Logic to handle the callback from Visa
-        // For example, verify the response data and update the transaction status accordingly
+        // Logic to handle the callback from MTN Mobile Money
     }
-@Override
-public Map<String, Object> transformToRENFormat(PaymentRequest paymentRequest) {
-    Map<String, Object> renRequest = new HashMap<>();
-    Map<String, Object> document = new HashMap<>();
-    Map<String, Object> accptrAuthstnReq = new HashMap<>();
-    Map<String, Object> hdr = new HashMap<>();
-    Map<String, Object> authstnReq = new HashMap<>();
-    Map<String, Object> envt = new HashMap<>();
-    Map<String, Object> acqrr = new HashMap<>();
-    Map<String, Object> mrchnt = new HashMap<>();
-    Map<String, Object> lctnAndCtct = new HashMap<>();
-    Map<String, Object> pstlAdr = new HashMap<>();
-    Map<String, Object> tx = new HashMap<>();
-    Map<String, Object> txDtls = new HashMap<>();
-    Map<String, Object> txId = new HashMap<>();
 
-    // Header Information
-    hdr.put("MsgFctn", "AUTQ");
-    hdr.put("PrtcolVrsn", "2.0");
-    hdr.put("CreDtTm", LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME));
+    @Override
+    public Map<String, Object> transformToRENFormat(PaymentRequest paymentRequest) {
+        Map<String, Object> renRequest = new HashMap<>();
+        Map<String, Object> document = new HashMap<>();
+        Map<String, Object> accptrAuthstnReq = new HashMap<>();
+        Map<String, Object> hdr = new HashMap<>();
+        Map<String, Object> authstnReq = new HashMap<>();
+        Map<String, Object> envt = new HashMap<>();
+        Map<String, Object> acqrr = new HashMap<>();
+        Map<String, Object> mrchnt = new HashMap<>();
+        Map<String, Object> lctnAndCtct = new HashMap<>();
+        Map<String, Object> pstlAdr = new HashMap<>();
+        Map<String, Object> tx = new HashMap<>();
+        Map<String, Object> txDtls = new HashMap<>();
+        Map<String, Object> txId = new HashMap<>();
 
-    // Environment
-    acqrr.put("Id", "configured_acquirer_id");
-    pstlAdr.put("TwnNm", paymentRequest.getBillingAddress().get("city"));
-    pstlAdr.put("CtryCd", paymentRequest.getBillingAddress().get("country"));
-    pstlAdr.put("PstCd", paymentRequest.getBillingAddress().get("zip"));
-    lctnAndCtct.put("PstlAdr", pstlAdr);
-    mrchnt.put("Id", "configured_merchant_id");
-    mrchnt.put("LctnAndCtct", lctnAndCtct);
-    envt.put("Acqrr", acqrr);
-    envt.put("Mrchnt", mrchnt);
+        // Header Information
+        hdr.put("MsgFctn", "AUTQ");
+        hdr.put("PrtcolVrsn", "2.0");
+        hdr.put("CreDtTm", LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME));
 
-    // Transaction Details
-    txDtls.put("TtlAmt", paymentRequest.getAmount());
-    txDtls.put("Ccy", paymentRequest.getCurrency());
-    tx.put("TxDtls", txDtls);
-    tx.put("TxTp", "CRDP");
-    txId.put("TxDtTm", LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME));
-    txId.put("TxRef", paymentRequest.getOrderId());
-    tx.put("TxId", txId);
+        // Environment
+        acqrr.put("Id", "configured_acquirer_id");
+        pstlAdr.put("TwnNm", paymentRequest.getBillingAddress().get("city"));
+        pstlAdr.put("CtryCd", paymentRequest.getBillingAddress().get("country"));
+        pstlAdr.put("PstCd", paymentRequest.getBillingAddress().get("zip"));
+        lctnAndCtct.put("PstlAdr", pstlAdr);
+        mrchnt.put("Id", "configured_merchant_id");
+        mrchnt.put("LctnAndCtct", lctnAndCtct);
+        envt.put("Acqrr", acqrr);
+        envt.put("Mrchnt", mrchnt);
 
-    // Assemble the request
-    authstnReq.put("Envt", envt);
-    authstnReq.put("Tx", tx);
-    accptrAuthstnReq.put("Hdr", hdr);
-    accptrAuthstnReq.put("AuthstnReq", authstnReq);
-    document.put("AccptrAuthstnReq", accptrAuthstnReq);
-    renRequest.put("Document", document);
+        // Transaction Details
+        txDtls.put("TtlAmt", paymentRequest.getAmount());
+        txDtls.put("Ccy", paymentRequest.getCurrency());
+        tx.put("TxDtls", txDtls);
+        tx.put("TxTp", "CRDP");
+        txId.put("TxDtTm", LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME));
+        txId.put("TxRef", paymentRequest.getOrderId());
+        tx.put("TxId", txId);
 
-    return renRequest;
+        // Assemble the request
+        authstnReq.put("Envt", envt);
+        authstnReq.put("Tx", tx);
+        accptrAuthstnReq.put("Hdr", hdr);
+        accptrAuthstnReq.put("AuthstnReq", authstnReq);
+        document.put("AccptrAuthstnReq", accptrAuthstnReq);
+        renRequest.put("Document", document);
+
+        return renRequest;
     }
 }
